7d48d4c4dc66f2873ce00eb135002005ef631eb8
roman-simionov <rsimionov@gmail.com>
2017-09-06 15:51:32 +0300

    fix default for workdays

--- js/viz/docs/doccharts.js
+++ js/viz/docs/doccharts.js
@@ -1016,9 +1016,9 @@ var dxChart = {
         * @name dxchartoptions_commonaxissettings_workdays
         * @publicName workdays
         * @type array
-        * @default undefined
+        * @default [ "monday", "tuesday", "wednesday", "thursday", "friday"]
         */
-        workdays: undefined,
+        workdays: ["monday", "tuesday", "wednesday", "thursday", "friday"],
         /**
         * @name dxchartoptions_commonaxissettings_holidays
         * @publicName holidays
